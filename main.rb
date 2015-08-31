b3b36b2e00fb353778c170a8d8b4c86e30c66949
Christopher Leabeter <christopherleabeter@gmail.com>
2015-08-31 14:14:22 -0400

    Deleted unused 'main.rb' file.

--- main.rb
+++
@@ -1,7 +0,0 @@
-require "rubygems"
-require "rest-client"
-
-
-wiki_url = "https://en.wikipedia.org/wiki/Main_Page"
-wiki_local_filename = "wiki-page.html"
-File.open(wiki_local_filename, "w") { |file| file.write(RestClient.get(wiki_url)) }
